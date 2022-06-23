bd327a76adc65328da4f7f623c17d415498c97e0
Laura Ludgero <lauraludgero@hotmail.com>
2022-06-22 23:54:56 -0400

    layout do timer

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,55 @@
 <template>
   <div class="container">
+    <div id="container__title">
+      <h1>Pomodoro</h1>
+    </div>
+    <img id="container__img" src="../assets/tomate.png" alt="" />
+    <div class="container__timer">
+        <h2>25:00</h2>
+      <button>Inciar</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Home'
-}
+  name: "Home",
+};
 </script>
 
 <style>
-.container{
-    height: 100vh;
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#container__title {
+  position: absolute;
+  margin-top: 10px;
+  left: 5%;
+}
+#container__img {
+  margin-top: 70px;
+}
+
+h2 {
+  font-size: 55px;
+  margin-top: 70px;
+  color: #fff;
+  text-align: center;
+}
+
+button {
+  background: #ef2b2b;
+  color: #fff;
+  margin-top: 90px;
+  width: 150px;
+  height: 55px;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  font-size: 30px;
+  text-align: center;
 }
 </style>
