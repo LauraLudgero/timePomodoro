9cc1acf5c05888b267816398ee8e2b75996aa435
Laura Ludgero <lauraludgero@hotmail.com>
2022-06-24 10:47:03 -0400

    Cronometro funcionando com botão de pausa

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,15 +5,98 @@
     </div>
     <img id="container__img" src="../assets/tomate.png" alt="" />
     <div class="container__timer">
-        <h2>25:00</h2>
-      <button>Inciar</button>
+      <path id="time" />
+
+      <div class="time-display">
+        <h2>{{ timeDisplay }}</h2>
+      </div>
+      <button @click="handleTimer">{{ buttonText }}</button>
     </div>
   </div>
 </template>
 
 <script>
+import ProgressBar from "progressbar.js";
+
 export default {
   name: "Home",
+  data: () => {
+    const pomodoroDuration = 0.1 * 60; // Cronomrtro 25 mim
+
+    return {
+      pomodoroDuration,
+      restDuration: 0.1 * 60,
+      currentTimeInSeconds: pomodoroDuration,
+      buttonText: "Start!",
+      timeP: null,
+      interval: null,
+    };
+  },
+  mounted: function () {
+    this.timeP = new ProgressBar.Path(
+      "#time",
+      (this.pomodoroDuration + 1) * 1000
+    ); //add um segundo e e converte para milis
+  },
+  methods: {
+    handleTimer() {
+      if (this.buttonText === "Start!" || this.buttonText === "Resume") {
+        this.animateBar();
+        this.buttonText = "Pause";
+      } else if (this.buttonText === "Pause") {
+        this.pauseBar();
+        this.buttonText = "Resume";
+      }
+    },
+    animateBar() {
+      this.reduceTime();
+      let segment = this.timeP;
+
+      segment.animate(
+        0,
+        {
+          duration: (this.currentTimeInSeconds + 1) * 1000,
+        },
+        this.onFinish
+      );
+    },
+    pauseBar() {
+      clearInterval(this.interval);
+    },
+    onFinish() {
+      if (this.currentTimeInSeconds <= 0) {
+        // Clear interval
+        clearInterval(this.interval);
+        this.startRest();
+      }
+    },
+    reduceTime() {
+      this.interval = setInterval(() => {
+        if (this.currentTimeInSeconds > 0) {
+          this.currentTimeInSeconds -= 1;
+        }
+      }, 1000);
+    },
+    startRest() {
+      // Set new interval
+      this.reduceTime();
+      setTimeout(() => {
+        clearInterval(this.interval);
+        this.currentTimeInSeconds = this.pomodoroDuration;
+        this.buttonText = "Start!";
+      });
+    },
+  },
+  computed: {
+    timeDisplay() {
+      const minutes = String(parseInt(this.currentTimeInSeconds / 60));
+      const seconds = String(this.currentTimeInSeconds % 60);
+      const paddedMinutes = ("0" + minutes).slice(-2);
+      const paddedSeconds = ("0" + seconds).slice(-2);
+
+      return `${paddedMinutes}:${paddedSeconds}`;
+    },
+  },
 };
 </script>
 
